03957fe977fd5fc0e195de38fd33d593fc937902
reflet <reflet.jp@gmail.com>
2017-08-14 09:11:24 +0900

    mysql.sql changed(add grant).

--- docker/initdb.mysql/mysql.sql
+++ docker/initdb.mysql/mysql.sql
@@ -1,20 +1,27 @@
 
---
--- Database: myblog
---
-create database myblog;
-use myblog;
+-- rootに権限追加
+grant all privileges on *.* to root@localhost identified by 'development' with grant option;
 
---
--- Table structure for table `posts`
---
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `summary` varchar(255) NOT NULL,
-  `body` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/**
+ * Sample SQL
+ * --
+ * -- Database: myblog
+ * --
+ * create database myblog;
+ * use myblog;
+ *
+ * --
+ * -- Table structure for table `posts`
+ * --
+ * CREATE TABLE `posts` (
+ *   `id` int(11) NOT NULL AUTO_INCREMENT,
+ *   `title` varchar(255) NOT NULL,
+ *   `summary` varchar(255) NOT NULL,
+ *   `body` text NOT NULL,
+ *   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ *   `updated_at` datetime NOT NULL,
+ *   PRIMARY KEY (`id`)
+ * ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+ */
+
 
